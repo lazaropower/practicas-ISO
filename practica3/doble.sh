5fe11c956447e75cf0ff646bd3b994e985793812
Lazaropower <lazaroesunagranpersona@gmail.com>
2017-10-16 15:29:13 +0200

    Añadidos ejercicios de la práctica 3

---
+++ practica3/doble.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -u
+echo "Introduzca un numero: "
+read a
+let b=$a**2
+echo "$a * $a = $b"
---
+++ practica3/num.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -u
+echo "Inicio del guion $0"
+echo "El PID de $0 es $$"
+echo "Fin del guion $0"
+exit 1
