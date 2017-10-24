bc50e4b027b2cc55b3abc049e11203265dc7026b
lazaropower <lazaroesunagranpersona@gmail.com>
2017-10-24 12:37:58 +0200

    Añadido dibujatexto.sh y dos casos de prueba para dos letras

---
+++ practica4/ejercicios_guiones/dibujatexto.sh
@@ -0,0 +1,22 @@
+#! /bin/bash -u
+
+[ $# -eq 0 ] && echo "Uso: $0 <fichero>" 1>&2 && exit 1
+[ ! -r $1 ] && echo "Imposible acceder al fichero $1" 1>&2 && exit 2
+
+while read linea 
+do 
+	i=1
+	car=r""
+	while [ ! -z "$car" ] # -z "$car" devuelve 0 si $car contiene ""
+	do
+		num=$(echo "$linea" | cut -c$i)
+		let i+=1
+		car=$(echo "$linea" | cut -c$i)
+		let i+=1
+		for j in $(seq 1 1 $num)
+		do
+			echo -n "$car"
+		done
+	done
+	echo
+done < $1
