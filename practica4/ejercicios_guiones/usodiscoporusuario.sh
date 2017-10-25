90db2b7944302029ee5716f8746cb228f34569fb
lazaropower <lazaroesunagranpersona@gmail.com>
2017-10-25 14:07:37 +0200

    Añadido más código de usodiscoporusuario.sh

--- practica4/ejercicios_guiones/usodiscoporusuario.sh
+++ practica4/ejercicios_guiones/usodiscoporusuario.sh
@@ -7,15 +7,31 @@ do
 	[ ! -d "$dir" ] && echo "$dir no es un directorio" 1>&2 && exit 2
 done
 
+errores=$(mktemp)
+usuarios=$(find "$@" -type f -printf "%u\n" 2> $errores | sort | uniq)
 ttotal=0
-find "$@" -type f -printf "%u\n" 2> /dev/null | uniq | (while read usuario
+for usu in $usuarios
 do
+	tamficheros=$(find "$@" -type f -user "$usu" -printf "%s\n" 2> $errores /dev/null)
 	tusuario=0
-	find "$@" -type f -user "$usuario" -printf "%s\n" 2> /dev/null 2> /dev/null | (while read tamano
+	for tam in $tamficheros
 	do
-		let tusuario+=$tamano
+		let tusuario+=$tam
 	done
-	let ttotal+=$tusuario	
-	echo "$usuario: $tusuario bytes")
+	let ttotal+=$tusuario
+	if [ $tusuario -lt 1048576 ] # 1 MiB = 1048576 bytes  
+	then
+		echo "$usu: $tusuario bytes"
+	else
+		let tusuario=\($tusuario/1024\)/1024
+		echo "$usu: $tusuario MiB"
+	fi
 done
-echo "Total de todos los usuarios: $ttotal bytes")
+if [ $ttotal -lt 1048576 ] 
+then
+	echo "Total de todos los usuarios: $ttotal bytes"
+else  
+	let ttotal=\($ttotal/1024\)/1024
+	echo "Total de todos los usuarios: $ttotal MiB"
+fi
+#TODO mostrar directorios de los que no se ha podido contabilizar su contenido.
