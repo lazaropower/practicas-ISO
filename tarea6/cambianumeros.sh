0759a2a3880374eeeb6cf4d56e0195fbe0429d59
lazaropower <lazaroesunagranpersona@gmail.com>
2017-10-26 19:27:46 +0200

    Terminado cambianumeros.sh, a falta de solventar un pequeño error que muestra una coma de más, pero no afecta a lo demás

--- tarea6/cambianumeros.sh
+++ tarea6/cambianumeros.sh
@@ -3,56 +3,78 @@
 [ $# -ne 2 ] && echo "Uso: $0 fichero1 fichero2" >&2 && exit 1
 [ ! -f $1 ] && echo "El fichero $1 no es un fichero regular o no existe" >&2 && exit 2
 
-# Función que comprueba si el último carácter de una palabra es un signo de puntuación. Devuelve 0(true) o 1(false)
-function isPunct {
-	aux=$(echo $palabra | rev | cut -c 1) 
-	echo $aux | grep -wq [[:punct:]]
-	if [ $? -eq 0 ]		
-	then
-		carpunct=$aux
-	fi
-}
 ncambios=0
-nlinea=0
+nlinea=1
+
 cat "$1" | (while read linea 
 do
-	npalabra=1	# Contador de palabras
+	haycambio=1		# Booleano que comprueba si ha habido algun cambio en la línea actual
+	npalabra=1		# Contador de palabras
 	palabra=" " 	# Palabra actual
-	# addpunct=1	# Booleano que indica si tenemos que añadir un signo de puntuación al final de la palabra		
+	impLinea=1		# Booleano que comprueba si se ha imprimdo ya "Linea $nlinea:"
 	while [ ! -z "$palabra" ]
 	do
-		carpunct=""	# Carácter de [:punct:] a añadir al final de la palabra.
+		ncambiosAux=$ncambios			# Guardamos el valor de $ncambios para un if posterior.
+		npalcambio=0					# Guarda el nº de palabra en la que ha habido un cambio.  
+		carpunct=""						# Carácter de [:punct:] a añadir al final de la palabra.
 		palabra=$(echo $linea | cut -f$npalabra -d" ")
-  	
+		aux=$(echo $palabra | rev | cut -c1)	# Último carácter de la palabra 
+		echo $aux | grep -wq [[:punct:]]		# Comprobamos si $aux carácter es un signo de puntuación
+		if [ $? -eq 0 ]
+		then
+			carpunct=$aux
+			palabra=$(echo $palabra | tr -d [:punct:])
+		fi
+  		
 		case $palabra in
-			"cero")	echo -n "0" >> $2 
+			"cero")	echo -n "0" >> $2
+				npalcambio=$npalabra
+				haycambio=0
 				let ncambios+=1
 				;;
-			"uno")	echo -n "1" >> $2
+			"uno"| "Uno")	echo -n "1" >> $2
+				npalcambio=$npalabra 
+				haycambio=0
 				let ncambios+=1
 				;;
 			"dos")	echo -n "2" >> $2
+				npalcambio=$npalabra
+				haycambio=0
 				let ncambios+=1
 				;;
 			"tres")	echo -n "3" >> $2
+				npalcambio=$npalabra 
+				haycambio=0 
 				let ncambios+=1
 				;;
 			"cuatro") echo -n "4" >> $2
+				npalcambio=$npalabra  
+				haycambio=0
 				let ncambios+=1	
 				;;
 			"cinco") echo -n "5" >> $2
+				npalcambio=$npalabra 
+				haycambio=0
 				let ncambios+=1	
 				;;
 			"seis")	echo -n "6" >> $2
+				npalcambio=$npalabra  
+				haycambio=0
 				let ncambios+=1
 				;;
 			"siete") echo -n "7" >> $2
+				npalcambio=$npalabra 
+				haycambio=0 
 				let ncambios+=1	
 				;;
 			"ocho")	echo -n "8" >> $2
+				npalcambio=$npalabra 
+				haycambio=0 
 				let ncambios+=1
 				;;
 			"nueve") echo -n "9" >> $2
+				npalcambio=$npalabra 
+				haycambio=0 
 				let ncambios+=1	
 				;;
 			*)	echo -n $palabra >> $2
@@ -60,11 +82,26 @@ do
 		esac
 		if [ ! -z $carpunct ]
 		then
-			echo -n $carpunct >> $2
+			echo -n $carpunct >> $2		# Si la palabra contenía un signo de puntuación, lo ponemos. 
 		fi
 	 	echo -n " " >> $2   # Metemos espacio al fichero. 
+		# Comprobamos si ha habido cambios para imprimir la línea. Si no ha habido ninguno, no se imprime.
+		if [ $ncambiosAux -lt $ncambios ]
+		then
+			if [ $impLinea -eq 1 ]
+			then 
+				echo -n "Línea $nlinea: "
+				impLinea=0
+			fi
+			echo -n "$npalcambio, "	
+		fi
 		let npalabra+=1
 	done
+	if [ $haycambio -eq 0 ]
+	then
+		echo
+	fi
 	echo >> $2	# Metemos salto de linea '\n' al fichero. 
 	let nlinea+=1	
-done)
+done
+echo "Número total de cambios: $ncambios")
